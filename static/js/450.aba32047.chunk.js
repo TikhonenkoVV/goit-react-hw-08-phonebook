8636d5f63578754be47312582aecc4797d586b4b
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-05-29 08:56:21 +0000

    Deploying to gh-pages from @ TikhonenkoVV/goit-react-hw-08-phonebook@4fb969a244e12187f355899945ea2bdc8a46c38c 🚀

---
+++ static/js/450.aba32047.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[450],{450:function(t,e,n){n.r(e);var c=n(2606),o=n(2943),a=n(9434),r=n(4296),i=n(184);e.default=function(){var t=(0,a.I0)();return(0,i.jsx)(c.$0,{children:(0,i.jsx)(c.W2,{children:(0,i.jsx)(o.t,{contact:{name:"",surname:"",number:"",email:"",img:""},title:"Add contact",onSetState:function(e,n){t((0,r.Mr)(n))}})})})}}}]);
+//# sourceMappingURL=450.aba32047.chunk.js.map
